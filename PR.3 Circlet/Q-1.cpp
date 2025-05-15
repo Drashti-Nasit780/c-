c3c14212a6167ac21c6ebdf1772a2cb07793e0b6
Drashti Nasit <drashtinasit780@gmail.com>
2025-05-15 09:24:37 +0530

    Update Q-1.cpp

---
+++ PR.3 Circlet/Q-1.cpp
@@ -0,0 +1,25 @@
+//Question:-1
+#include<iostream>
+using namespace std;
+
+main()
+{
+	//input:
+	int i,j;
+	
+	for(i=41; i<=45; i++){
+         for(j=41; j<=i; j++){
+			cout<<j;
+		 }
+	     
+           cout<<endl;
+   }
+	     
+}
+   //output:
+   
+//41
+//41 42
+//41 42 43
+//41 42 43 44
+//41 42 43 44 45
